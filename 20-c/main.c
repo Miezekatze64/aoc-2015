7c124160f6dce1c609fa163f904c23b935a3ae18
Miezezkatze64 <miezemoon@gmail.com>
2022-02-22 09:53:32 +0100

    finished day 20

--- 20-c/main.c
+++ 20-c/main.c
@@ -1,6 +1,10 @@
-#include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
+
+int min(int a, int b) {
+    if (a < b) return a;
+    return b;
+}
 
 void main(int argc, char **argv) {
     const long N = 29000000;
@@ -15,8 +19,23 @@ void main(int argc, char **argv) {
     for (int i = 0; i < N/10; i++) {
         if (houses[i] >= N) {
             printf("Part 1: %d\n", i);
-            free(houses);
-            return;
+            break;
+        }
+    }
+
+    free(houses);
+    houses = malloc(N/10 * sizeof(int));
+    
+    for (int i = 1; i < N/10; i++) {
+        for (int j = i; j <= min(i*50, N/10); j += i) {
+            houses[j] += 11*i;
+        }
+    }
+
+    for (int i = 0; i < N/10; i++) {
+        if (houses[i] >= N) {
+            printf("Part 2: %d\n", i);
+            break;
         }
     }
 
