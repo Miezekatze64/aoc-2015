5d8f5181f6e6c38965e42731ace98fd40c34a68b
Miezezkatze64 <miezemoon@gmail.com>
2022-02-23 12:25:32 +0100

    added day 25

---
+++ 25-java/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static final int ROW = 3010;
+    public static final int COL = 3019;
+
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    public Main() {
+        int x = 1;
+        int y = 1;
+        long val = 20151125L;
+
+        while (x != COL || y != ROW) {
+            val = next(val);
+            if (y == 1) {
+                y = x+1;
+                x = 1;
+            } else {
+                y--;
+                x++;
+            }
+        }
+        System.out.println("Part 1: " + val);
+    }
+
+    private long next(long prev) {
+        return (prev*252533L)%33554393L;
+    }
+}
